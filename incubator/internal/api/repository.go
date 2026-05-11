91af89069288ccef6cf586a62e20fc2ea6f7da3a
DKS <danielsaisani@gmail.com>
2026-05-11 21:23:23 +0100

    chore: add docstring to api package

--- incubator/internal/api/repository.go
+++ incubator/internal/api/repository.go
@@ -1,3 +1,4 @@
+// Package api contains all the contracts that bespoke service dependencies must implement and conform to in order to be usable by the services. Communication between components should be via these contracts so that components remain swappable and there is no unncessary tight coupling
 package api
 
 import "context"
