4e7a7cf48a98272fe543ee077626faa5d5b941cc
DKS <danielsaisani@gmail.com>
2026-05-15 07:46:55 +0100

    chore

--- incubator/internal/api/scraper.go
+++ incubator/internal/api/scraper.go
@@ -3,11 +3,22 @@ package api
 import "context"
 
 type Scraper interface {
-	Posts() <-chan *Story // CreatorService reads from this
+	Posts() <-chan *Story // CreatorService reads from this and pushes it to the StoryRepository
 	Run(ctx context.Context) error
 }
 
 type Story struct {
-	Title string
-	Body  string
+	Title  string
+	Body   StoryBody
+	NSFW   bool
+	Posted bool
+}
+
+type StoryBody struct {
+	Body string
+}
+
+// Populated returns whether the body actually exists to distinguish between posts that don't have any content and those that do.
+func (sb StoryBody) Populated() bool {
+	return len(sb.Body) > 0
 }
